9d8687f1968442184526dda3cf904a7ed74a2aea
Alexey <forer@yandex.ru>
2018-06-22 23:13:26 +0300

    Init

---
+++ src/Figure.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Figure extends JComponent
+{
+    private Color color;
+    Figure(Color color) {
+        this.color = color;
+        setOpaque(false);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.setColor(color);
+        g.fillRect(0, 0, 25, 25);
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+
+/**
+ *
+ * Класс, отвечающий за работу всего приложения(всей игры)
+ *
+ */
+public class Game {
+
+    public static void main(String args[]) {
+        new TestPainer();
+
+    }
+
+}
